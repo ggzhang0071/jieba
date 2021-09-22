779798d26358d12b251ec42c1b5a4f8311772628
93 machine <155051843@qq.com>
2021-09-22 14:01:38 +0800

    znext 词性分析

--- test.py
+++ test.py
@@ -1,20 +1,23 @@
-# encoding=utf-8
-import jieba
+from os import POSIX_FADV_NOREUSE
 
-jieba.enable_paddle()# 启动paddle模式。 0.40版之后开始支持，早期版本不支持
-strs=["我来到北京清华大学","乒乓球拍卖完了","中国科学技术大学"]
-for str in strs:
-    seg_list = jieba.cut(str,use_paddle=True) # 使用paddle模式
-    print("Paddle Mode: " + '/'.join(list(seg_list)))
 
-seg_list = jieba.cut("我来到北京清华大学", cut_all=True)
-print("Full Mode: " + "/ ".join(seg_list))  # 全模式
+keywords=[]
+for i in range(10):
+    keywords.append([])
+    keywords[i].append(tags)
 
-seg_list = jieba.cut("我来到北京清华大学", cut_all=False)
-print("Default Mode: " + "/ ".join(seg_list))  # 精确模式
+repeated=dict()
+for i  in  range(len(keywords)):
+     for j in range(len(keywords[0])):
+        keyword=keywords[i][j]
+        if  keyword in repeated.keys():
+            repeat[keyword].append(i)
+        else:
+            repeated[keyword]=[i]
 
-seg_list = jieba.cut("他来到了网易杭研大厦")  # 默认是精确模式
-print(", ".join(seg_list))
+            
+for  key,val in  repeated.items():
+    if len(val)>1:
+        print(key,len(val))
 
-seg_list = jieba.cut_for_search("小明硕士毕业于中国科学院计算所，后在日本京都大学深造")  # 搜索引擎模式
-print(", ".join(seg_list))
+
--- znext_analysis.py
+++ znext_analysis.py
@@ -4,9 +4,25 @@ import jieba.analyse
 path="znext_data/question_cn_processed.json"
 
 with open(path,"r",encoding="utf-8") as fid:
-    data=json.load(fid)
+    data=json.load(fid,strict=False)
     
-for topic in data:
-    tags = jieba.analyse.extract_tags(topic, topK=10)
+keywords=[]
+for i,topic in enumerate(data):
+    tags = jieba.analyse.extract_tags(topic["Text"], topK=5,allowPOS=("n","vn"))
+    #tags=jieba.analyse.TextRank(topic["Text"])
+    keywords.append(tags)
+
+repeated=dict()
+for i  in  range(len(keywords)):
+     for j in range(len(keywords[i])):
+        keyword=keywords[i][j]
+        if  keyword in repeated.keys():
+            repeated[keyword].append(i)
+        else:
+            repeated[keyword]=[i]
+
+for  key,val in  repeated.items():
+    if len(val)>1:
+        print(key,val)        
 
     
